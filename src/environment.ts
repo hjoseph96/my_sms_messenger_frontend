599273398037f8c14b62cc1c51f6a3c1166ce9df
Herbert Joseph <techniquea2z@gmail.com>
2025-05-26 13:50:17 -0400

    fix(netlify): use ng build

--- src/environment.ts
+++ src/environment.ts
@@ -2,8 +2,8 @@ import { isDevMode } from '@angular/core';
 
 let env = {
     production: false,
-    // baseUrl: 'https://my-sms-messenger-api-8fac18b6169c.herokuapp.com'  
-    baseUrl: 'http://localhost:3001'
+    baseUrl: 'https://my-sms-messenger-api-8fac18b6169c.herokuapp.com'  
+    // baseUrl: 'http://localhost:3001'
 };
 
 
